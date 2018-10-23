2cc4400c83473092e4764dc21aebf004641626b5
HenryLi2077 <sos4491@gmail.com>
2018-10-23 16:57:40 +0800

    Add basic spawnpoints

---
+++ Assets/Scripts/TriggerSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSpawn : MonoBehaviour {
+
+    public GameObject[] spawnPoints;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            foreach (GameObject spawnPoint in spawnPoints)
+            {
+                Debug.Log("123");
+
+                spawnPoint.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (GameObject spawnPoint in spawnPoints)
+            {
+                spawnPoint.SetActive(false);
+            }
+        }
+    }
+}
